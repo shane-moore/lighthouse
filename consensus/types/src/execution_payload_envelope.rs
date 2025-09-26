d977abe13700de53fc2c99b0cc65b48fc0d3e5dc
shane-moore <skm1790@gmail.com>
2025-09-26 16:56:50 -0700

    gloas envelope helpers

--- consensus/types/src/execution_payload_envelope.rs
+++ consensus/types/src/execution_payload_envelope.rs
@@ -4,6 +4,7 @@ use beacon_block_body::KzgCommitments;
 use derivative::Derivative;
 use serde::de::{Deserializer, Error as _};
 use serde::{Deserialize, Serialize};
+use ssz::{Decode, DecodeError};
 use ssz_derive::{Decode, Encode};
 use superstruct::superstruct;
 use test_random_derive::TestRandom;
@@ -72,6 +73,19 @@ impl<'a, E: EthSpec> ExecutionPayloadEnvelopeRef<'a, E> {
 
     pub fn block_hash(&self) -> ExecutionBlockHash {
         self.payload().block_hash()
+    }
+}
+
+impl<E: EthSpec> ExecutionPayloadEnvelope<E> {
+    /// Custom SSZ decoder that takes a `ForkName` as context.
+    pub fn from_ssz_bytes_for_fork(bytes: &[u8], fork_name: ForkName) -> Result<Self, DecodeError> {
+        match fork_name {
+            ForkName::Gloas => ExecutionPayloadEnvelopeGloas::from_ssz_bytes(bytes)
+                .map(ExecutionPayloadEnvelope::Gloas),
+            _ => Err(DecodeError::BytesInvalid(format!(
+                "ExecutionPayloadEnvelope does not support fork {fork_name:?}"
+            ))),
+        }
     }
 }
 
